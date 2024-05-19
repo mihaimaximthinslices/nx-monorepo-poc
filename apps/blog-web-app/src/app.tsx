bdd1caf69bfe578c9619ca6c168b652953f7f75c
mihaimaximthinslices <135857206+mihaimaximthinslices@users.noreply.github.com>
2024-05-19 15:09:33 +0300

    Update app.tsx

--- apps/blog-web-app/src/app.tsx
+++ apps/blog-web-app/src/app.tsx
@@ -4,7 +4,7 @@ import '@aws-amplify/ui-react/styles.css';
 import { generateClient } from 'aws-amplify/data';
 import { Schema } from '@amplify-monorepo/backend';
 import { useEffect, useState } from 'react';
-import outputs from './amplify_outputs.json';
+import outputs from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure(outputs);
