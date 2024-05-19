d422967922793c7a775f78aac6583fe42925d10c
mihaimaximthinslices <135857206+mihaimaximthinslices@users.noreply.github.com>
2024-05-19 16:03:13 +0300

    Add sales app

--- apps/sales-web-app/src/main.tsx
+++ apps/sales-web-app/src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { App } from './app';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
